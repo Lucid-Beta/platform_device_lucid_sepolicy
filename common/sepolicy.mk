pumpac7i0n/android_device_pa_sepolicy

sepolicy: add system-only directory to common and qcom
Change-Id: Ic32d6e86e2796225b894ca5a56280773036c2cbf

common/sepolicy.mk
@@ -17,7 +17,8 @@ BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
 
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/pa/sepolicy/common/dynamic
+    device/pa/sepolicy/common/dynamic \
+    device/pa/sepolicy/common/system-only
 else
 BOARD_SEPOLICY_DIRS += \
     device/pa/sepolicy/common/dynamic \

qcom/sepolicy.mk
@@ -14,7 +14,8 @@ BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
 
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/pa/sepolicy/qcom/dynamic
+    device/pa/sepolicy/qcom/dynamic \
+    device/pa/sepolicy/qcom/system-only
 else
 BOARD_SEPOLICY_DIRS += \
     device/pa/sepolicy/qcom/dynamic \

Lucid-Beta/platform_device_lucid_sepolicy

common/sepolicy.mk
@@ -17,7 +17,8 @@ BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
 
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/lucid/sepolicy/common/dynamic
+    device/lucid/sepolicy/common/dynamic \
+    device/lucid/sepolicy/common/system-only
 else
 BOARD_SEPOLICY_DIRS += \
     device/lucid/sepolicy/common/dynamic \

qcom/sepolicy.mk
@@ -14,7 +14,8 @@ BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
 
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/lucid/sepolicy/qcom/dynamic
+    device/lucid/sepolicy/qcom/dynamic \
+    device/lucid/sepolicy/qcom/system-only
 else
 BOARD_SEPOLICY_DIRS += \
     device/lucid/sepolicy/qcom/dynamic \